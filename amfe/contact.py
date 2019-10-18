624534336111bf6f88d5c1dc19ce2330bc29584e
makast <makast@ethz.ch>
2019-10-18 16:08:50 +0200

    Fixed formatting in contact.

--- amfe/contact.py
+++ amfe/contact.py
@@ -5,9 +5,8 @@
 #
 """
 Module for contact handling withing the FE context.
-
 Up to now, only contact that does not require additional dofs is implemented. The contact model under consideration uses
-a return mapping algorithm, i.e. it requires the previous state. For now, this information is saved in the contact model.
+a return mapping algorithm, i.e. it requires the previous state. For now, this info is saved in the contact model.
 The contact model should be able to return the forces and contributions to the Jacobian.
 For now, contact depends only on the displacements.
 Different implementations might use velocity and will have contributions that are not in the stiffness matrix K.
@@ -20,9 +19,7 @@ __all__ = ['Contact',
            'JenkinsContact'
            ]
 
-
 use_fortran = False
-
 
 
 # use_fortran = False
@@ -47,14 +44,12 @@ class Contact:
             observer.update()
 
 
-
-
-
 class HysteresisContact(Contact):
     """
     Base class for contacts that require the previous state and only need the displacements for computation.
     Follows the implementation ideas of Afzal 2016.
     """
+
     def __init__(self):
         super().__init__()
 
@@ -77,17 +72,20 @@ class HysteresisContact(Contact):
         """
         pass
 
+
 class JenkinsContact(HysteresisContact):
     r"""
     2D Jenkins element that models contact in a plane.
 
-    The contact is detected along the normal direction (z) and contact forces are computed with a linear penalty approach.
+    The contact is detected along the normal direction (z) and contact forces are computed with a
+    linear penalty approach.
     The friction force is computed in the plane with the Masing-Hypothesis, Jenkins formulation of the Hysteresis.
 
     see Afzal 2016  for a derivation of this  contact model.
 
     """
-    def __init__(self, k_n=30,  k_t = 30, mu=0.3, u_rel_t= None, f_t = None):
+
+    def __init__(self, k_n=30, k_t=30, mu=0.3, u_rel_t=None, f_t=None):
         """
 
                 Parameters
@@ -95,7 +93,8 @@ class JenkinsContact(HysteresisContact):
                 k_n : float
                     Penalty stiffness in normal direction
                 k_t : float
-                    Stiction/Spring stiffness in tangential direction. TODO: Potentially shape (2,) for non-isotropic friction
+                    Stiction/Spring stiffness in tangential direction.
+                    TODO: Potentially shape (2,) for non-isotropic friction
                 mu : float
                     Friction coefficient of the Coulomb limit
 
@@ -116,21 +115,20 @@ class JenkinsContact(HysteresisContact):
         self.mu = mu
 
         if u_rel_t is None or f_t is None:
-            self._u_rel_t = np.zeros(2,1)
-            self._f_t = np.zeros(2, 1)
+            self._u_rel_t = np.zeros((2, 1))
+            self._f_t = np.zeros((2, 1))
         else:
             self._u_rel_t = u_rel_t
             self._f_t = f_t
 
         self._update_states()
 
-
     def __repr__(self):
         """
         repr(obj) function for smart representing for debugging
         """
-        return 'amfe.contact.JenkinsContact(%f,%f)'\
-            % (self.k_n,  self.mu)
+        return 'amfe.contact.JenkinsContact(%f,%f)' \
+               % (self.k_n, self.mu)
         # TODO: Write a nice presentation for the Jenkins element
 
     def _update_states(self):
@@ -140,53 +138,48 @@ class JenkinsContact(HysteresisContact):
         self._u_rel_t_previous = self._u_rel_t
         self._f_t_previous = self._f_t
 
+    def K_and_f(self, u_rel):
 
-    def  K_and_f(self, u_rel):
-
-        K = np.zeros((3,3))
-        f = np.zeros((3,1))
+        K = np.zeros((3, 3))
+        f = np.zeros((3, 1))
 
         self._u_rel_t = u_rel[:2]
 
         # Check if we are actually in contact by looking at the normal dof.
         if u_rel[2] < 0:
             # We are in contact
-            f[2] = self.k_n *u_rel[2]
-            K[2,2] = self.k_n
+            f[2] = self.k_n * u_rel[2]
+            K[2, 2] = self.k_n
 
             # Compute trial stress
-            trial_stress = self._f_t_previous + self.k_t *(self._u_rel_t_previous - self._u_rel_t)
+            trial_stress = self._f_t_previous + self.k_t * (self._u_rel_t_previous - self._u_rel_t)
 
             # Check if we are in the sticking or slipping regime
 
-            f_limit = f[2]* self.mu  # Coulomb friction limit
-            f_trial = np.linalg.norm(trial_stress,ord =2)
+            f_limit = f[2] * self.mu  # Coulomb friction limit
+            f_trial = np.linalg.norm(trial_stress, ord=2)
 
             if f_trial <= f_limit:
                 # Sticking regime
                 self._f_t = trial_stress
                 f[:2] = self._f_t
-                K[0,0] = self.k_t
-                K[1,1] = self.k_t
+                K[0, 0] = self.k_t
+                K[1, 1] = self.k_t
+
             else:
                 # Slipping regime
                 # Be careful not to divide by zero
                 if f_trial > 0:
-                    self._f_t = trial_stress/f_trial * f_limit
+                    self._f_t = trial_stress / f_trial * f_limit
                     # Derivative with respect to normal displacement
-                    K[:2,2] =  trial_stress/f_trial * self. k_n* self.mu
+                    K[:2, 2] = trial_stress / f_trial * self.k_n * self.mu
                     # Derivative with respect to tangential displacements
-                    K[:2,:2] = self.k_t *f_limit/f_trial**3 * np.array([[trial_stress[1]**2, trial_stress[0]*trial_stress[1] ],
-                                                              [trial_stress[0]*trial_stress[1], trial_stress[0]**2]])
+                    K[:2, :2] = self.k_t * f_limit / f_trial ** 3 * \
+                                np.array([[trial_stress[1] ** 2, trial_stress[0] * trial_stress[1]],
+                                          [trial_stress[0] * trial_stress[1], trial_stress[0] ** 2]])
 
                 else:
-                    self._f_t = np.zeros((2,1)) # We should never reach this statement
+                    self._f_t = np.zeros((2, 1))  # We should never reach this statement
                     print("Warning, this statement should not be reachable. Slipping regime with 0 trial stress?!")
 
         return K, f
-
-
-
-
-
-
